141332ca78040e0f734210396c96c9d587456e3c
Yudi Xue <10211+binarycrayon@users.noreply.github.com>
2020-09-12 14:54:29 -0700

    [argo-2.10.0] Setup pytest for integ test
    Signed-off-by: Yudi Xue <10211+binarycrayon@users.noreply.github.com>

---
+++ tests/workflows/test_workflow_from_yaml.py
@@ -0,0 +1,27 @@
+import requests
+import yaml
+from argo.workflows.client import V1alpha1Api
+from argo.workflows.config import load_kube_config
+
+NAMESPACE = "argo"
+
+load_kube_config()  # loads local configuration from ~/.kube/config
+
+
+
+# hello-world example
+
+
+
+HELLO_WORLD_YML = 'https://raw.githubusercontent.com/argoproj/argo/v2.10.1/examples/hello-world.yaml'
+
+def test_submit_hello_world_workflow():
+    resp = requests.get(HELLO_WORLD_YML)
+    resp.raise_for_status()
+
+    manifest: dict = yaml.safe_load(resp.text)
+    # TODO: V1Container default value for name should be ''
+    manifest['spec']['templates'][0]['container']['name'] = ''
+    v1alpha1 = V1alpha1Api()
+    # Submit the Workflow to the `argo` namespace
+    v1alpha1.create_namespaced_workflow(NAMESPACE, manifest)
