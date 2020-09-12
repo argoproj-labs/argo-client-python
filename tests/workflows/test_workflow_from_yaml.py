94d6d4a2a2811aad7b5a70c4b68d21c652583eff
Yudi Xue <10211+binarycrayon@users.noreply.github.com>
2020-09-12 16:07:25 -0700

    [argo-2.10.0] add more tests
    Signed-off-by: Yudi Xue <10211+binarycrayon@users.noreply.github.com>

--- tests/workflows/test_workflow_from_yaml.py
+++ tests/workflows/test_workflow_from_yaml.py
@@ -7,16 +7,33 @@ NAMESPACE = "argo"
 
 load_kube_config()  # loads local configuration from ~/.kube/config
 
-
-
-# hello-world example
-
-
-
 HELLO_WORLD_YML = 'https://raw.githubusercontent.com/argoproj/argo/v2.10.1/examples/hello-world.yaml'
+CRON_WORKFLOW_YML = 'https://raw.githubusercontent.com/argoproj/argo/v2.10.1/examples/cron-workflow.yaml'
+CORN_FLIP_YML = 'https://raw.githubusercontent.com/argoproj/argo/v2.10.1/examples/coinflip.yaml'
+
 
 def test_submit_hello_world_workflow():
     resp = requests.get(HELLO_WORLD_YML)
+    resp.raise_for_status()
+
+    manifest: dict = yaml.safe_load(resp.text)
+    v1alpha1 = V1alpha1Api()
+    # Submit the Workflow to the `argo` namespace
+    v1alpha1.create_namespaced_workflow(NAMESPACE, manifest)
+
+
+def test_submit_cron_workflow():
+    resp = requests.get(CRON_WORKFLOW_YML)
+    resp.raise_for_status()
+
+    manifest: dict = yaml.safe_load(resp.text)
+    v1alpha1 = V1alpha1Api()
+    # Submit the Workflow to the `argo` namespace
+    v1alpha1.create_namespaced_cronworkflow(NAMESPACE, manifest)
+
+
+def test_submit_corn_flip_workflow():
+    resp = requests.get(CORN_FLIP_YML)
     resp.raise_for_status()
 
     manifest: dict = yaml.safe_load(resp.text)
